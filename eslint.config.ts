f9efdf9ba09ae34992a8b1ebb81aa422d4e9132c
Silvio Clécio <silvioprog@gmail.com>
2026-01-19 10:37:43 -0300

    chore: configure ESLint rule to disallow 'waitForTimeout' with custom message

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,6 +2,7 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import playwright from "eslint-plugin-playwright";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
@@ -14,6 +15,21 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2022,
       globals: globals.browser
+    }
+  },
+  {
+    files: ["e2e/**/*.ts"],
+    plugins: { playwright },
+    settings: {
+      playwright: {
+        messages: {
+          noWaitForTimeout:
+            "Avoid page.waitForTimeout() as it leads to flaky tests. Use locator.waitFor(), expect().toBeVisible(), or other condition-based waits instead."
+        }
+      }
+    },
+    rules: {
+      "playwright/no-wait-for-timeout": "error"
     }
   }
 ]);
